3caca864ca76ebb96595708bb5e740e11a425874
Jonathan Sprauel <jonathan.sprauel@thalesaleniaspace.com>
2018-10-08 20:52:34 +0200

    adding end of run card

--- convert.bat
+++ convert.bat
@@ -1,3 +1,7 @@
+:loop
+
 for /f %%i in ('python ./generate.py') do set source_svg=%%i
-set INKSCAPE_PATH="C:\Program Files\Inkscape\inkscape.exe"
-cmd /C "%INKSCAPE_PATH% -z -f %source_svg% -w 2160 -j -e %source_svg%.png"
+set INKSCAPE_PATH="C:\Program Files\ImageMagick-7.0.7-Q16\convert.exe"
+cmd /C "%INKSCAPE_PATH% -density 1200 %source_svg% %source_svg%.png"
+
+goto loop
